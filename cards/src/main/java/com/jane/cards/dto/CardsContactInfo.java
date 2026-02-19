748622f7b9e8f4bedf0f57b814627a97ba6baea4
Ritu Bafna <ritujane78@gmail.com>
2026-02-19 11:24:16 +0545

    rename fields

--- cards/src/main/java/com/jane/cards/dto/CardsContactInfo.java
+++ cards/src/main/java/com/jane/cards/dto/CardsContactInfo.java
@@ -5,6 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
+@ConfigurationProperties(prefix = "cards")
 public record CardsContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 }
--- loans/src/main/java/com/jane/loans/dto/LoansContactInfo.java
+++ loans/src/main/java/com/jane/loans/dto/LoansContactInfo.java
@@ -5,6 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
+@ConfigurationProperties(prefix = "loans")
 public record LoansContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 }
