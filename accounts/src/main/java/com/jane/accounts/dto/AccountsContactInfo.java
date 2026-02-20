017ce606a5c70f39ed011f1211b3833151f2c5bb
Ritu Bafna <ritujane78@gmail.com>
2026-02-20 17:24:18 +0545

    add spring bus, rabbitmq, hookdeck for all 3 microservices

--- accounts/src/main/java/com/jane/accounts/dto/AccountsContactInfo.java
+++ accounts/src/main/java/com/jane/accounts/dto/AccountsContactInfo.java
@@ -1,10 +1,19 @@
 package com.jane.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfo {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
+//public record AccountsContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//}
