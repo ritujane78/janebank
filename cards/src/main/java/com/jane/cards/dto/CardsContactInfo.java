53d8aefc81cdc04357c42589531f0c31aa43c40f
Ritu Bafna <ritujane78@gmail.com>
2026-02-20 17:24:45 +0545

    add spring bus, rabbitmq, hookdeck for all 3 microservices

--- cards/src/main/java/com/jane/cards/dto/CardsContactInfo.java
+++ cards/src/main/java/com/jane/cards/dto/CardsContactInfo.java
@@ -1,10 +1,19 @@
 package com.jane.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfo {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
+//public record CardsContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//}
--- loans/src/main/java/com/jane/loans/dto/LoansContactInfo.java
+++ loans/src/main/java/com/jane/loans/dto/LoansContactInfo.java
@@ -1,10 +1,20 @@
 package com.jane.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansContactInfo
+{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
+//public record LoansContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//}
