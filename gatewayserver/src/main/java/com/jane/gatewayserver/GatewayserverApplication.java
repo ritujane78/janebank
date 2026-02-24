a9b840e5c00824d2fbf04d62607b36e48a18b810
Ritu Bafna <ritujane78@gmail.com>
2026-02-24 13:39:46 +0545

    add /janebank/** prefix in all the paths routed from the gateway server

--- gatewayserver/src/main/java/com/jane/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jane/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,32 @@ package com.jane.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator janeBankRouteConfig(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/janebank/accounts/**")
+						.filters(f -> f.rewritePath("/janebank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/janebank/loans/**")
+						.filters(f -> f.rewritePath("/janebank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/janebank/cards/**")
+						.filters(f -> f.rewritePath("/janebank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTSCARDS")).build();
 	}
 
 }
