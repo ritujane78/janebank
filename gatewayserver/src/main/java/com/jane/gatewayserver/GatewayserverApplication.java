b9b3ae0fd5ecf908d426b34ab8ccce1eabb3c6cb
Ritu Bafna <ritujane78@gmail.com>
2026-02-25 18:25:57 +0545

    implement circuit breaker in gateway server

--- gatewayserver/src/main/java/com/jane/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jane/gatewayserver/GatewayserverApplication.java
@@ -21,17 +21,21 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/janebank/accounts/**")
 						.filters(f -> f.rewritePath("/janebank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/janebank/loans/**")
 						.filters(f -> f.rewritePath("/janebank/loans/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("loansCircuitBreaker")))
 						.uri("lb://LOANS"))
 				.route(p -> p
 						.path("/janebank/cards/**")
 						.filters(f -> f.rewritePath("/janebank/cards/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("cardsCircuitBreaker")))
 						.uri("lb://CARDS")).build();
 	}
 
