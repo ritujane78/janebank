2c0e12cdc55723aba030583c7729c8d134fbfc4e
Ritu Bafna <ritujane78@gmail.com>
2026-03-04 12:51:20 +0545

    use spring cloud functions instead of REST features

---
+++ message/src/main/java/com/jane/message/dto/AccountsMsgDto.java
@@ -0,0 +1,4 @@
+package com.jane.message.dto;
+
+public record AccountsMsgDto(Long accountNumber, String name, String email, String phoneNumber) {
+}
---
+++ message/src/main/java/com/jane/message/functions/MessageFunctions.java
@@ -0,0 +1,31 @@
+package com.jane.message.functions;
+
+import com.jane.message.dto.AccountsMsgDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class MessageFunctions {
+
+    Logger logger = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountsMsgDto, AccountsMsgDto> email() {
+        return accountsMsgDto -> {
+            logger.info("Sending email with the detail: " + accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDto, Long> sms() {
+        return accountsMsgDto -> {
+            logger.info("Sending sms with the detail: " + accountsMsgDto.toString());
+            return accountsMsgDto.accountNumber();
+        };
+    }
+}
